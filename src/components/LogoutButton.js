49c05143f068e7ac3f87656132a7d5eb518c3f8e
enrico-favale <enrico.favale@outlook.it>
2025-08-24 17:34:07 +0200

    logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,12 +1,14 @@
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LogoutButton() {
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
-      logout();
-      window.location.href = "/"; // Reindirizza alla home (opzionale)
+      await logout(); // Assicurati che logout sia async
+      navigate("/"); // Reindirizza alla home senza ricaricare la pagina
     } catch (error) {
       console.error("Errore durante il logout:", error);
     }
@@ -15,7 +17,7 @@ export default function LogoutButton() {
   return (
     <button
       onClick={handleLogout}
-      className={`inline-flex items-center justify-center hover:bg-primary/90 dark:hover:bg-primary/90 hover:text-white hover:scale-105 border border-ring rounded-md p-2 text-md px-4 bg-card text-card-foreground hover:shadow-card transition-all duration-200`}
+      className="inline-flex items-center justify-center hover:bg-primary/90 dark:hover:bg-primary/90 hover:text-white hover:scale-105 border border-ring rounded-md p-2 text-md px-4 bg-card text-card-foreground hover:shadow-card transition-all duration-200"
     >
       Logout
     </button>
